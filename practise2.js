6248708cedf2eb2bb3ec2fe20afb45767a019d61
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2023-07-31 19:49:51 +0600

    practise3 added

--- practise2.js
+++ practise2.js
@@ -119,4 +119,16 @@ else {
     largestNumber = sum3;
 }
 
+var marks = [13, 15, 14, 20, 18];
+
+for (var i = 0; i < marks.length; i++) {
+  if (marks[i] >= 15) {
+    continue;
+  }
+  console.log(marks[i]);
+}
+
+var name = "false";
+
+
 
---
+++ practise3.js
@@ -0,0 +1,83 @@
+// task 1
+
+// for (var i = 1; i <= 39; i++) {
+//     console.log(i);
+//     console.log('ajke amar mon valo ney');
+
+    
+// }
+
+// task 2
+// for(var i = 58; i <= 98; i++){
+//     console.log(i);
+// }
+// task 3
+// for (let i = 412; i <= 456; i+=2) {
+//     console.log(i);
+    
+// }
+// task 4
+// for (let i = 581; i <= 623; i+=2) {
+//     console.log(i);
+    
+// }
+// task 5
+// while loop
+// var i = 1;
+// while( i<= 10){
+//     console.log(i);
+//     console.log('apple khabo');
+//     i++
+// }
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+//     console.log('gaza daw ');
+// }
+
+// task 6
+
+// var learned = ['html', 'css', 'tailwind', 'dasiy sawri', 'bootstrap','java'];
+
+// console.log(learned);
+
+// task 7 
+
+// var phoneName = ['ipon','nokla','sawomi','samsung','moltrla','sympony','realme'];
+// var i = 0;
+
+// while (i < phoneName.length) {
+    
+//     console.log(phoneName[i]);
+//     i++;
+
+// }
+
+// for(var i = 0; i< phoneName.length; i++){
+//     var element = phoneName[i];
+//     console.log(element);
+// }
+
+// task 8
+
+// for( i = 30; i <= 86; i++){
+//     if(i > 35){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// task 9
+var bookPrice = [50,60,10,150,100,199,199.99,201,200, 220,290,250 ];
+
+
+for(var i = 0; i <bookPrice.length; i++){
+    
+    var element = bookPrice[i];
+
+    if(element > 200){
+        break;
+    }
+
+    console.log(element);
+}
