0a6257b873aaf54e64e939eb1d8377051b1dcaf1
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2023-08-08 13:51:12 +0600

    updated

--- isphotoname.js
+++ isphotoname.js
@@ -5,14 +5,12 @@ function isValidPhotoName(photoName, imageExtention){
     else{
         for(let i = 0; i < imageExtention.length; i++){
             const element = imageExtention[i]; 
-            if(photoName.endsWith(element.length)){
+            if(photoName+element.endsWith(element.length)){
                 return true;                
             } 
-            else{
-                return false;
-            }        
+                   
         }
-       
+       return false;
     }
 }   
 
@@ -21,3 +19,23 @@ const extention = ['.jpg','.jpeg','.png','.ico'];
 
 const isImageExtention = isValidPhotoName(imageName, extention);
 console.log(isImageExtention);
+
+// function isValidPhotoName(photoName, imageExtensions) {
+//     if (typeof photoName !== 'string' || !Array.isArray(imageExtensions)) {
+//         return 'Please provide a valid photo name and image extensions array.';
+//     } else {
+//         for (let i = 0; i < imageExtensions.length; i++) {
+//             const extension = imageExtensions[i];
+//             if (photoName+extension.endsWith(extension)) {
+//                 return true;
+//             }
+//         }
+//         return false; // Moved the return statement outside of the loop
+//     }
+// }
+
+// const imageName = 'image'; // Fixed typo: 'iamge' to 'image'
+// const extensions = ['.jpg', '.jpeg', '.png', '.ico']; // Renamed 'extention' to 'extensions'
+
+// const isImageExtension = isValidPhotoName(imageName, extensions);
+// console.log(isImageExtension);
