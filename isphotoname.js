9355e4309b4d40249346b60f093b78e701c235b6
FAZLE RABBI <mdfazlerabbim621@gmail.com>
2023-08-08 18:32:16 +0600

    added

--- isphotoname.js
+++ isphotoname.js
@@ -1,24 +1,24 @@
-function isValidPhotoName(photoName, imageExtention){
-    if(typeof photoName !== 'string' || !Array.isArray(imageExtention)){
-        return 'please provide me a valid string'
-    }
-    else{
-        for(let i = 0; i < imageExtention.length; i++){
-            const element = imageExtention[i]; 
-            if(photoName+element.endsWith(element.length)){
-                return true;                
-            } 
+// function isValidPhotoName(photoName, imageExtention){
+//     if(typeof photoName !== 'string' || !Array.isArray(imageExtention)){
+//         return 'please provide me a valid string'
+//     }
+//     else{
+//         for(let i = 0; i < imageExtention.length; i++){
+//             const element = imageExtention[i]; 
+//             if(photoName+element.endsWith(element.length)){
+//                 return true;                
+//             } 
                    
-        }
-       return false;
-    }
-}   
+//         }
+//        return false;
+//     }
+// }   
 
-const imageName = 'iamge';
-const extention = ['.jpg','.jpeg','.png','.ico'];
+// const imageName = 'iamge';
+// const extention = ['.jpg','.jpeg','.png','.ico'];
 
-const isImageExtention = isValidPhotoName(imageName, extention);
-console.log(isImageExtention);
+// const isImageExtention = isValidPhotoName(imageName, extention);
+// console.log(isImageExtention);
 
 // function isValidPhotoName(photoName, imageExtensions) {
 //     if (typeof photoName !== 'string' || !Array.isArray(imageExtensions)) {
@@ -39,3 +39,8 @@ console.log(isImageExtention);
 
 // const isImageExtension = isValidPhotoName(imageName, extensions);
 // console.log(isImageExtension);
+
+
+
+let cube = Math.pow(4,3);
+console.log(cube);
